5687bb0b0aff89772caac21d90ac47a5f8d37afd
OrzuNarziyev <86144583+OrzuNarziyev@users.noreply.github.com>
2021-06-19 20:01:03 +0500

    lesson1

--- lesson1.py
+++ lesson1.py
@@ -1,2 +1,84 @@
-print("hello world")
-print("salom dunyo")
+
+# a = 2
+# b = 4
+# c = a+b
+
+#x,y,z = 'salom',3,4.5
+# x=y=z="hello world"
+
+# print(x,y,z,sep="+")
+# print(a)
+# print("1+2+3 = ",end="")
+# print(1+2+3)
+# x=15
+# x//=3  #x=x+3
+# print(int(x))
+# x=5
+# x**=3 #x=x**3
+# print(x)
+# a = False
+# b = 3
+# print(a >= b)
+# print(a or b)
+# a=11
+# print (not(a>3 or a<10))
+
+# x = ["olma", "banan"]
+# y = ["olma", "banan"]
+# z = y
+# print(x is not y)
+
+# x = ["audi", "mustang"]
+# print("audi" in x) #true
+# print("audi" not in x) #False
+# a = float(input("a="))
+# b = float(input("b="))
+# q = a+b
+# k = a*b
+# bol = a/b
+# ayir = a-b
+# print(f"qoshish {q} ")
+# a = int(input("soni"))
+# a = "salom"
+# print(3*a)
+
+# starlar biLan ishlash
+# list = [1,3,4,6,3,3,2]
+
+# print(list[-4:4])
+
+# print(str[:])
+# sum  = 0
+# for x in str:
+#     sum+=1
+#     print(sum)
+#     # print(x,end="\n")
+# print(sum)
+# print("Gulla yashna hur \"O\'zbekistonim\"")
+# print(f"salom \t qudrat")
+# name = input("ismingiz")
+# age = int(input("yoshingiz"))
+# print("salom men {} . men {} yoshdaman".format(name,age) )
+# str = "salom boqiy gijduvan"
+# str1 = str.upper()
+
+# print(str.title())       #bosh harfni katta harfga almashtiradi
+# print(str1.lower())            #kichik harflarga ogiradi
+# print(mystr.upper())            katta harflarga ogiradi
+# print(str.swapcase())        # katta harfni kichikka ogiradi va aksincha
+# print(mystr.title())            har bir sozni bosh harf bilan ogiradi
+# print(str.count('o'))         #shu iboradan nechtaligini sanaydi    ///  4   ///
+# print(str.endswith(''))   #oxirgi so'z shu bolsa tru qaytaradi   /// true ///
+str = "salom boqiy buxoro"
+
+# print(str.startswith('The'))     #birinchi soz shu bolsa tru qaytaradi
+
+# print(str.find('buxoro'))
+# print(str.index('o'))            # ///   15  ////
+# a="str"
+# print(a.isdigit())
+# print(str.rjust(30, '+'))
+# mystr = '         The, quik brown fox jumps over the lazy dog'
+# print(mystr.strip())  # The quik brown fox jumps over the lazy dog
+# mystr = 'The, quik brown fox jumps over the lazy dog'
+# print(mystr.partition('ik'))
